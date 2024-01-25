d865d99cd3ec1bf0693c46bc0ded68439ebe22b0
dassepascal <pascaldasse56@gmail.com>
2024-01-25 15:34:20 +0100

    modif StudentCard + User pour composer stan

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasOne; // Add this import statement
+
+use App\Models\StudentCard;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -42,4 +44,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+   /**
+     * @return HasOne<StudentCard>
+     */
+
+    public function studentCard()
+    {
+        return $this->hasOne(StudentCard::class);
+    }
 }
