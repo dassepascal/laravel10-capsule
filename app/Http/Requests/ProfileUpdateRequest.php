fdee885419e325e2370a7b7e55b1858806e7937f
dassepascal <pascaldasse56@gmail.com>
2024-01-25 14:54:33 +0100

    test span ok

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rule; // Add this line
 
 class ProfileUpdateRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array<int,string|\Illuminate\Contracts\Validation\Rule>|string>
      */
     public function rules(): array
     {
